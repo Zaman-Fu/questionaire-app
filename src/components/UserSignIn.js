3a12ea04c9c5d5b57e4861dd95b9f9610162d34c
Jorge Ruiz <jorge@macdiego.com>
2022-11-08 12:04:00 +0100

    bootstrap + routing + axios

--- src/components/UserSignIn.js
+++ src/components/UserSignIn.js
@@ -1,41 +1,35 @@
 //import './UserSignIn.css';
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UserSignIn = () => {
+  const [formEmail, setEmail] = useState("");
 
-    const [formEmail, setEmail] = useState("");
+  const emailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault(); //avoid page reload to handle this request with javascript
+    //Now sign the user in.
+    console.log("Form Submission!");
 
-    const emailChangeHandler = (event) => {
-        setEmail(event.target.value);
-    }
-    const submitHandler = (event) => {
-        event.preventDefault();//avoid page reload to handle this request with javascript
-        //Now sign the user in.
-        console.log('Form Submission!');
-    };
+    window.location = "/questions";
+  };
 
-    return (
-        <div className="formdiv">
-            <p>Input email to proceed to questions</p>
-
-            <form className="signin-form" onSubmit={submitHandler}>
-                
-                <label >Email:</label>
-                <input type="text" id="email" name="email" onChange={emailChangeHandler}></input>
-                <input type="submit" value="Submit" onSubmit={submitHandler }></input> 
-
-
-            </form>
-
-
-        </div>
-
-
-    );
-}
-
-
-
-
+  return (
+    <div className="formdiv">
+      <p>Input email to proceed to questions</p>
+      <form className="signin-form" onSubmit={submitHandler}>
+        <label>Email:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={emailChangeHandler}
+        ></input>
+        <input type="submit" value="Submit" onSubmit={submitHandler}></input>
+      </form>
+    </div>
+  );
+};
 
 export default UserSignIn;
